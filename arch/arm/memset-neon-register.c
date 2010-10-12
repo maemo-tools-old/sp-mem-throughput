484e17a9d21ae489fce4a38845bbee1ef44178da
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2010-10-12 15:08:51 +0300

    NEON memset routines: VSTM & VST1 & PLD variants
    Introduce more memory reading routines that utilize either VSTM or VST1
    instructions, and routines that implement preloading.

--- arch/arm/memset-neon-register.c
+++ arch/arm/memset-neon-register.c
@@ -23,18 +23,60 @@
 
 #include "routine.h"
 
-void *memset_neon_16(void *, int, size_t);
-void *memset_neon_32(void *, int, size_t);
-void *memset_neon_64(void *, int, size_t);
-void *memset_neon_128(void *, int, size_t);
+void *memset_vstm_8(void *, int, size_t);
+void *memset_vstm_16(void *, int, size_t);
+void *memset_vstm_32(void *, int, size_t);
+void *memset_vstm_64(void *, int, size_t);
+void *memset_vstm_128(void *, int, size_t);
+void *memset_vstm_256(void *, int, size_t);
 
-ROUTINE_REGISTER_MEMSET(memset_neon_16,
-	"ARM NEON memset, 16 byte aligned writes")
-ROUTINE_REGISTER_MEMSET(memset_neon_32,
-	"ARM NEON memset, 32 byte aligned writes")
-ROUTINE_REGISTER_MEMSET(memset_neon_64,
-	"ARM NEON memset, 64 byte aligned writes")
-ROUTINE_REGISTER_MEMSET(memset_neon_128,
-	"ARM NEON memset, 128 byte aligned writes")
+void *memset_vstm_pld_8(void *, int, size_t);
+void *memset_vstm_pld_16(void *, int, size_t);
+void *memset_vstm_pld_32(void *, int, size_t);
+void *memset_vstm_pld_64(void *, int, size_t);
+void *memset_vstm_pld_128(void *, int, size_t);
+void *memset_vstm_pld_256(void *, int, size_t);
+
+void *memset_vst1_8(void *, int, size_t);
+void *memset_vst1_16(void *, int, size_t);
+void *memset_vst1_32(void *, int, size_t);
+void *memset_vst1_64(void *, int, size_t);
+void *memset_vst1_128(void *, int, size_t);
+void *memset_vst1_256(void *, int, size_t);
+
+void *memset_vst1_pld_8(void *, int, size_t);
+void *memset_vst1_pld_16(void *, int, size_t);
+void *memset_vst1_pld_32(void *, int, size_t);
+void *memset_vst1_pld_64(void *, int, size_t);
+void *memset_vst1_pld_128(void *, int, size_t);
+void *memset_vst1_pld_256(void *, int, size_t);
+
+ROUTINE_REGISTER_MEMSET(memset_vstm_8,   "NEON VSTM, 8 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_16,  "NEON VSTM, 16 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_32,  "NEON VSTM, 32 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_64,  "NEON VSTM, 64 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_128, "NEON VSTM, 128 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_256, "NEON VSTM, 256 bytes per loop")
+
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_8,   "NEON VSTM+PLD, 8 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_16,  "NEON VSTM+PLD, 16 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_32,  "NEON VSTM+PLD, 32 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_64,  "NEON VSTM+PLD, 64 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_128, "NEON VSTM+PLD, 128 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vstm_pld_256, "NEON VSTM+PLD, 256 bytes per loop")
+
+ROUTINE_REGISTER_MEMSET(memset_vst1_8,   "NEON VST1, 8 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_16,  "NEON VST1, 16 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_32,  "NEON VST1, 32 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_64,  "NEON VST1, 64 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_128, "NEON VST1, 128 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_256, "NEON VST1, 256 bytes per loop")
+
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_8,   "NEON VST1+PLD, 8 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_16,  "NEON VST1+PLD, 16 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_32,  "NEON VST1+PLD, 32 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_64,  "NEON VST1+PLD, 64 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_128, "NEON VST1+PLD, 128 bytes per loop")
+ROUTINE_REGISTER_MEMSET(memset_vst1_pld_256, "NEON VST1+PLD, 256 bytes per loop")
 
 #endif /* __ARM_NEON__ */
