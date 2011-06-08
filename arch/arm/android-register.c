186888fd2ffdefd3e0900cabf7af3a9920e7d7a8
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2011-06-08 15:15:35 +0300

    Adopt android strcpy() for sp-mem-throughput

--- arch/arm/android-register.c
+++ arch/arm/android-register.c
@@ -5,10 +5,12 @@
 void *android_memset(void *, int, size_t);
 void *android_memcpy(void *, const void *, size_t);
 size_t android_strlen(const char *);
+char *android_strcpy(char *, const char *);
 int android_memcmp(const void *, const void *, size_t);
 
 ROUTINE_REGISTER_MEMSET(android_memset, "Android bionic memset")
 ROUTINE_REGISTER_MEMCPY(android_memcpy, "Android bionic memcpy")
 ROUTINE_REGISTER_STRLEN(android_strlen, "Android bionic strlen")
+ROUTINE_REGISTER_STRCPY(android_strcpy, "Android bionic strcpy")
 
 #endif /* __ARM_NEON__ */
